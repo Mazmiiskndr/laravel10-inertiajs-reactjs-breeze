ff35e7b64e4ed3bb97402abdeecbe79605588796
arjungunawan <arjun.gnw09@gmail.com.com>
2023-05-02 10:49:23 +0700

    simple about page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Auth;
@@ -34,6 +35,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/about', [AboutController::class, 'index'])->name('about');
 });
 
 require __DIR__.'/auth.php';
